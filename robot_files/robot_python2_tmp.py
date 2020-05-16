20ac676b1634f78f83cd6f824fe84a0b780d624e
irsbugs <irsbugs@hotmail.com>
2020-05-16 16:49:40 +1200

    Update robot_python2_tmp.py

--- robot_files/robot_python2_tmp.py
+++ robot_files/robot_python2_tmp.py
@@ -1,76 +1,51 @@
-import sys
-
+import sys;
 
 def output(s):
-    sys.stdout.write(s + "\n")
-    sys.stdout.flush()
-
+  sys.stdout.write(s + "\n");
+  sys.stdout.flush();
 
 def input():
-    line = sys.stdin.readline()
-    if line == "quit\n":
-        sys.exit(say("Quit"))
-    return line
-
+  l = sys.stdin.readline();
+  if l == "quit\n": exit(0);
+  return l;
 
 def left():
-    output("left")
-    input()
-
+  output("left"); input();
 
 def right():
-    output("right")
-    input()
-
+  output("right"); input();
 
 def forward():
-    output("forward")
-    input()
-
+  output("forward"); input();
 
 def take():
-    output("take")
-    input()
-
+  output("take"); input();
 
 def drop():
-    output("drop")
-    input()
-
+  output("drop"); input();
 
 def escape():
-    output("escape")
-    input()
-
+  output("escape"); input();
 
 def say(s):
-    output("say " + s)
-    input()
+  output("say " + s); input();
 
-
-Void = 0
-Wall = 1
-Rock = 2
-Web = 3
-Exit = 4
-Unknown = 5
-
+Void = 0;
+Wall = 1;
+Rock = 2;
+Web = 3;
+Exit = 4;
+Unknown = 5;
 
 def look():
-    output("look")
-    ans = input()
-    if (ans == "void\n"):
-        return Void
-    if (ans == "wall\n"):
-        return Wall
-    if (ans == "rock\n"):
-        return Rock
-    if (ans == "web\n"):
-        return Web
-    if (ans == "exit\n"):
-        return Exit
-    return Unknown
+  output("look");
+  ans = input();
+  if (ans == "void\n"): return Void;
+  if (ans == "wall\n"): return Wall;
+  if (ans == "rock\n"): return Rock;
+  if (ans == "web\n"): return Web;
+  if (ans == "exit\n"): return Exit;
+  return Unknown;
 
-
-output("start")
+output("start");
 input()
